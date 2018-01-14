08fd5df876a5814f5a96cb76ab1f2002223fd8a9
Michael Daye <mgdaye@gmail.com>
2018-01-13 19:40:37 -0500

    changed img size and refactored layout with flexbox

---
+++ portfolio/src/components/Title.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Title = props =>(
+    <div className={"jumbotron jumbotron-fluid text-center bg-success"}>
+        <h1 className={"font-weight-bold"}>Mike Daye</h1>
+        <h3 className={"font-weight-bold"}>Developer | <span className={"font-italic"}>Designer</span></h3>
+    </div>
+);
+
+export default Title;
